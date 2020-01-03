c59af4b2d5f058776b4262e88825bf1c9c7bd03a
PelcMichal <michal.pelc@assist.cz.>
2020-01-03 15:55:57 +0100

    k

--- Projekt/src/projekt/Main.java
+++ Projekt/src/projekt/Main.java
@@ -18,6 +18,16 @@ public class Main {
 		
 		try {
 			Connection conn = DriverManager.getConnection(config.DatabaseConfig.getUrl(), config.DatabaseFileUtils.getInstance().getProperties());
+			
+			
+			Statement stmt = conn.createStatement();
+			//stmt.execute("CREATE TEMP TABLE geomtest(mycirc circle)");
+			ResultSet rs = stmt.executeQuery("SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME='mytablename'");
+			if (rs.next()) 
+			{
+				System.out.println("t");
+			}
+			System.out.println("f");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			LOGGER.error(e);
