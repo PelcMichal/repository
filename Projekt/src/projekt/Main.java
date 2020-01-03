675f49f90c91f4f2a60aab9b71ba01804fe87beb
PelcMichal <michal.pelc@assist.cz.>
2020-01-03 16:22:25 +0100

    fg

--- Projekt/src/projekt/Main.java
+++ Projekt/src/projekt/Main.java
@@ -21,12 +21,16 @@ public class Main {
 			
 			
 			Statement stmt = conn.createStatement();
-			//stmt.execute("CREATE TEMP TABLE geomtest(mycirc circle)");
+			//stmt.execute("CREATE DATABASE Projekt");
+			//stmt.execute("CREATE TABLE Projekt.kmen(int id unique not null, VARCHAR(50), int mnozstvi)");
+			//stmt.execute("CREATE TABLE Projekt.zmeny(int id, int id_kmen unique not null, VARCHAR(50), int mnozstvi,)");
+			//stmt.execute("CREATE kmen TABLE geomtest(mycirc circle)");
 			ResultSet rs = stmt.executeQuery("SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME='mytablename'");
 			if (rs.next()) 
 			{
-				System.out.println("t");
+				System.out.println(rs);
 			}
+			System.out.println(rs);
 			System.out.println("f");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
