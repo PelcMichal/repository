e878c393ca2b16b3ae96ace1a031b8401cc7d42e
PelcMichal <michal.pelc@assist.cz.>
2020-01-06 10:27:25 +0100

    i

--- Projekt/src/projekt/Main.java
+++ Projekt/src/projekt/Main.java
@@ -1,11 +1,14 @@
 package projekt;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
-import config.DatabaseFileUtils;
-import exception.PropertyFileException;
 import database.DatabaseFacade;
 
 public class Main {
@@ -19,25 +22,68 @@ public class Main {
 		
 		LOGGER.debug("*** BEGIN ***");
 		
-		
+		/*
+		CREATE TABLE kmen(id serial unique,jmeno VARCHAR (50) NOT NULL,mnozstvi integer NOT NULL)
+		CREATE TABLE zmeny(id serial PRIMARY KEY,id_kmen serial NOT NULL,jmeno VARCHAR (50),zmena_mnozstvi integer NOT NULL,odeslana TIMESTAMPTZ DEFAULT Now(),zpracovano BOOLEAN DEFAULT false)
+		CREATE TABLE historie(id serial PRIMARY KEY,id_kmen serial NOT NULL,id_zmeny serial NOT NULL,jmeno VARCHAR (50) NOT NULL,zmena_mnozstvi integer NOT NULL,bylo_provedeno BOOLEAN NOT NULL,zpracovano TIMESTAMPTZ DEFAULT Now())
+		 */
 		try {
 			Connection conn = DriverManager.getConnection(config.DatabaseConfig.getUrl(), config.DatabaseFileUtils.getInstance().getProperties());
 			
 			Statement stmt = conn.createStatement();
 			//stmt.execute("CREATE DATABASE projekt");
 			//stmt.execute("DROP DATABASE projekt");
-			//stmt.execute("DROP table zmeny");
+			stmt.execute("DROP table kmen");
+			stmt.execute("DROP table zmeny");
+			stmt.execute("DROP table historie");
 			DatabaseFacade.tryToCreate(conn);
 			
+			//stmt.execute("INSERT INTO kmen(id, jmeno, mnozstvi) VALUES (1, 'a', 1)");
+			//stmt.execute("INSERT INTO zmeny(id_kmen, jmeno, zmena_mnozstvi) VALUES (1, 'a', 1)");
+			
+			stmt.execute(DatabaseFacade.createZmena(1, "lopata", 10));
+			stmt.execute(DatabaseFacade.createZmena(1, "lopata", -5));
+			stmt.execute(DatabaseFacade.createZmena(1, "lopata", -10));
+			stmt.execute(DatabaseFacade.createZmena(1, "lopata", 5));
+			stmt.execute(DatabaseFacade.createZmena(2, "krumpac", 5));
 			
 			
-			ResultSet rs = stmt.executeQuery("SELECT * FROM zmeny WHERE zpracovano = false");
-			if (rs.next()) 
+			//DatabaseFacade.vipis(conn);
+			ResultSet z = stmt.executeQuery("SELECT id_kmen,jmeno,zmena_mnozstvi FROM zmeny WHERE zpracovano = false");
+			
+			while (z.next()) 
 			{
-				System.out.println(rs);
+				
+				ResultSet k = stmt.executeQuery("SELECT id,jmeno,mnozstvi FROM kmen WHERE id = "+z.getInt(1));
+				if (k.next())
+				{
+					if(k.getInt(3)+z.getInt(3)>=0)
+					{
+						stmt.execute(DatabaseFacade.updateKmen(z.getInt(1), z.getInt(3), k.getInt(3)));
+						stmt.execute(DatabaseFacade.createHistory(z.getInt(1), z.getInt(3), true, k.getString(2)));
+						LOGGER.debug("Added to "+ k.getString(2));
+					}
+					else
+					{
+						stmt.execute(DatabaseFacade.zmenaZpracovana(z.getInt(1)));
+						stmt.execute(DatabaseFacade.createHistory(z.getInt(1), z.getInt(3), false, k.getString(2)));
+						LOGGER.debug("Not enough "+ k.getString(2));
+					}
+				}
+				else
+				{System.out.println(z.getNString(2));
+					System.out.println("AAAAAAA");
+					stmt.execute(DatabaseFacade.createKmen(z.getInt(1), z.getString(2), z.getInt(3)));System.out.println("AAAAAAA");
+					stmt.execute(DatabaseFacade.zmenaZpracovana(z.getInt(1)));
+					stmt.execute(DatabaseFacade.createHistory(z.getInt(1), z.getInt(3), true, z.getString(2)));
+					LOGGER.debug("Created "+z.getString(2));
+				}
+				
+				
+				//System.out.println(z.getInt(1)+z.getString(2)+z.getInt(3));
+				LOGGER.debug("Chenge finished");
 			}
-			System.out.println(rs);
-			System.out.println("f");
+			//DatabaseFacade.vipis(conn);
 			conn.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
